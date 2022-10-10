1e4580a0cd276bd436bf3580361f80b3d6ed1060
Eduard Andar <andar.edd@gmail.com>
2022-10-10 15:34:35 +0200

    bash scripts update

---
+++ send_tx_cosmos_already_imported.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+VALIDATOR_ADDRESS=$(gaiad keys show cosmt -a)
+gaiad tx bank send $VALIDATOR_ADDRESS cosmos10rpv2hdmw5mta0akn9sg2uud45hhc5l7jm3fle 10000uatom --fees=15000uatom --gas=auto --gas-adjustment=1.20 --chain-id=cosmoshub-4 --node=https://rpc.cosmos.network:443
+
+
--- send_tx_osmo.sh
+++ send_tx_osmo.sh
@@ -3,5 +3,5 @@
 #your seed goes here
 'share flame stumble mutual ivory wool fun burden hill car van bid' | osmosisd keys add cosmst --recover
 VALIDATOR_ADDRESS=$(osmosisd keys show cosmst -a)
-yes | osmosisd tx bank send $CSMT_VALIDATOR_ADDRESS osmo10rpv2hdmw5mta0akn9sg2uud45hhc5l76qzeft 200000uosmo --chain-id osmosis-1 --fees 20000uosmo --gas auto
+yes | osmosisd tx bank send $CSMT_VALIDATOR_ADDRESS osmo10rpv2hdmw5mta0akn9sg2uud45hhc5l76qzeft 200000uosmo --chain-id osmosis-1 --fees 20000uosmo --gas auto -gas-adjustment=1.15
 
